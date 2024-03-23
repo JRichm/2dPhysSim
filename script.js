fda361d250d0cabc5792e3c80393bd44fb7d7328
James Richmond <jblackenmusic@gmail.com>
2024-03-22 21:32:15 -0600

    implemented vector2 class

--- script.js
+++ script.js
@@ -11,15 +11,35 @@ let movingDown = false;
 let friction = 0.1;
 
 
+class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(v) {
+        return new Vector(this.x + v.x, this.y + v.y);
+    }
+    
+    subtract(v) {
+        return new Vector(this.x - v.x, this.y - v.y);
+    }
+
+    multiply(n) {
+        return new Vector(this.x * n, this.y * n)
+    }
+
+    magnitude() {
+        return Math.sqrt(this.x**2 + this.y**2);
+    }
+}
+
 class Ball {
     constructor(posX, posY, radius) {
-        this.posX = posX;
-        this.posY = posY;
+        this.pos = new Vector(posX, posY)
         this.radius = radius;
-        this.velX = 0;
-        this.velY = 0;
-        this.accX = 0;
-        this.accY = 0;
+        this.vel = new Vector(0, 0)
+        this.acc = new Vector(0, 0)
         this.acceleration = 1;
         this.player = false;
         balls.push(this);
@@ -27,7 +47,7 @@ class Ball {
 
     drawBall() {
         ctx.beginPath();
-        ctx.arc(this.posX, this.posY, this.radius, 0, 2*Math.PI);
+        ctx.arc(this.pos.x, this.pos.y, this.radius, 0, 2*Math.PI);
         ctx.strokeStyle = 'black';
         ctx.stroke();
         ctx.fillStyle = 'red';
@@ -36,18 +56,17 @@ class Ball {
 
     displayVelAcc() {
         ctx.beginPath();
-        ctx.moveTo(this.posX, this.posY);
-        ctx.lineTo(this.posX + this.accX * 100, this.posY + this.accY * 100);
+        ctx.moveTo(this.pos.x, this.pos.y);
+        ctx.lineTo(this.pos.x + this.acc.x * 100, this.pos.y + this.acc.y * 100);
         ctx.strokeStyle = 'green';
         ctx.stroke();
 
         ctx.beginPath();
-        ctx.moveTo(this.posX, this.posY);
-        ctx.lineTo(this.posX + this.velX * 10, this.posY + this.velY * 10);
+        ctx.moveTo(this.pos.x, this.pos.y);
+        ctx.lineTo(this.pos.x + this.vel.x * 10, this.pos.y + this.vel.y * 10);
         ctx.strokeStyle = 'blue';
         ctx.stroke();
     }
-
 }
 
 function keyController(ball) {
@@ -82,34 +101,38 @@ function keyController(ball) {
     })
 
     if (movingLeft) {
-        ball.accX = -ball.acceleration;
+        ball.acc.x = -ball.acceleration;
     }
     if (movingUp) {
-        ball.accY = -ball.acceleration;
+        ball.acc.y = -ball.acceleration;
     }
     if (movingRight) {
-        ball.accX = ball.acceleration;
+        ball.acc.x = ball.acceleration;
     }
     if (movingDown) {
-        ball.accY = ball.acceleration;
+        ball.acc.y = ball.acceleration;
     }
 
     if (!movingUp && !movingDown) {
-        ball.accY = 0;
+        ball.acc.y = 0;
     }
 
     if (!movingLeft && !movingRight) {
-        ball.accX = 0;
+        ball.acc.x = 0;
     }
 
-    ball.velX += ball.accX;
-    ball.velY += ball.accY;
+    ball.vel = ball.vel.add(ball.acc)
+    ball.vel = ball.vel.multiply(1 - friction)
+    ball.pos = ball.pos.add(ball.vel)
 
-    ball.velX *= 1 - friction;
-    ball.velY *= 1 - friction;
+    // ball.vel.x += ball.acc.x;
+    // ball.vel.y += ball.acc.y;
 
-    ball.posX += ball.velX;
-    ball.posY += ball.velY;
+    // ball.vel.x *= 1 - friction;
+    // ball.vel.y *= 1 - friction;
+
+    // ball.pos.x += ball.vel.x;
+    // ball.pos.y += ball.vel.y;
 }
 
 
